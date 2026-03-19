a5cd341fc5b5a6e823ea291180a3008afd1f1e2d
EdielRodrigues <diel_zi_nho25@hotmail.com>
2026-03-19 01:49:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req,res)=>{
+res.send("online");
+});
+
 // 🔐 FIREBASE
 admin.initializeApp({
 credential: admin.credential.applicationDefault(),
